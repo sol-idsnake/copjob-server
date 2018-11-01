0e35fd6447d7efb0bc1c7fb55e658ab9d18beef7
Hendrik Wendt <h.wendt@gmx.com>
2018-11-01 10:00:39 -0400

    initialized

---
+++ departments/models.js
@@ -0,0 +1,39 @@
+"use strict";
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const departmentSchema = mongoose.Schema({
+  name: { type: String },
+  location: { type: String },
+  state: { type: String },
+  position: { type: String },
+  salary: { type: String },
+  age: { type: Number },
+  citizenship: { type: Boolean },
+  url: {
+    link: { type: String },
+    tag: { type: String }
+  },
+  description: { type: String }
+});
+
+departmentSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    name: this.name,
+    location: this.location,
+    state: this.state,
+    position: this.position,
+    salary: this.salary,
+    age: this.age,
+    citizenship: this.citizenship,
+    url: this.url,
+    description: this.description
+  };
+};
+
+// first arg passed to model is capital and singular, whereas mongo looks for the same name in plural and
+// non-capitalized in the collections. E.G 'POST', but the collections name is 'posts'
+const Department = mongoose.model("Department", departmentSchema);
+
+module.exports = { Department };
---
+++ departments/router.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const { Department } = require("./models");
+
+const app = express();
+const jsonParser = bodyParser.json();
+app.use(express.json());
+
+router.get("/get", (req, res) => {
+  Department.find()
+    .then(departments => {
+      res.json(departments.map(department => department.serialize()));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
+
+router.post("/add", jsonParser, (req, res) => {
+  Department.create({
+    name: req.body.name,
+    location: req.body.location,
+    state: req.body.state,
+    position: req.body.position,
+    salary: req.body.salary,
+    age: req.body.age,
+    citizenship: req.body.citizenship,
+    url: req.body.url,
+    description: req.body.description
+  })
+    .then(department => {
+      console.log(department + " before serialize");
+      res.status(201).json(department.serialize());
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Can't create department" });
+    });
+});
+
+module.exports = router;
