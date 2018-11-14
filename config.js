4964abc34979a73996675fcd17637b255117c26d
unknown <h.wendt@gmx.com>
2018-11-14 09:07:46 -0500

    fixed db url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 module.exports = {
-  DATABASE_URL: process.env.DATABASE_URL || "mongodb://localhost/copbjob-react",
+  DATABASE_URL: process.env.DATABASE_URL || "mongodb://localhost/copjob-react",
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000"
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
 };
