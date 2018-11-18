e803a47c9cf335d45927a68bdac3adfda825789a
Hendrik Wendt <h.wendt@gmx.com>
2018-11-18 15:12:43 -0500

    updated CORS

--- server.js
+++ server.js
@@ -10,12 +10,13 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header("Access-Control-Allow-Credentials", true);
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
   }
